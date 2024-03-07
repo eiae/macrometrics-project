76778a99e382795015a81fdd521df614dde1417c
eiae <erikignasi@gmail.com>
2024-03-07 12:35:19 +0100

    refactor BVAR main code

--- VAR.m
+++ VAR.m
@@ -8,38 +8,53 @@ close all
 clc
 seed=0;  
 rng(seed);  
-
  
-% load the mixed frequency data
-%load DataMF
-%y = [GDP IPI HICP CORE Euribor1Y UNRATE]; % variables  to  be  used
-lags = 2;                                 % number  of  lags 
+% model specs
+lags = 2;  % number of lags
+options.mf_varindex = 1;  % position of target variable
+options.K = 1000;  % total iterations
+options.priors.name = 'Minnesota';  % prior type with standard hyperparams
+options.noprint = 1;
 
-% T aggregation: the quarterly variable  
-% xq(t) = 1/3( xm(t) + xm(t-1) + xm(t-2)) at least two lags are needed
-options.mf_varindex     = 1; 
-options.K               = 1000;   
-options.priors.name     = 'Minnesota';
-options.noprint         = 1;
-bvarmf                  = bvar_(yVAR,lags,options); % estimate the var model
-
-sorty = sort(bvarmf.yfill,3);   % sort the smoothed  state
+% given we are working with level data the M2Q match is
+% Yq(t) = 1/3*Ym(t) + 1/3*Ym(t-1) + 1/3*Ym(t-2)
+m2q = [(1/3);(1/3);(1/3)];  % month-to-quarter conversion
+L = length(m2q);  % length of m2q conversion (additional lags from mapping) 
 
 
-% plot: levels
-figure('Name','Monthly EA GDP')
-plot(dates,yVAR(:,1),'Linewidth',2,'marker','o','color','r'); hold on; 
-plot(dates,sorty(:,1,0.5*bvarmf.ndraws),'b','Linewidth',2)
-legend('Quarterly data','MXFREQ VAR flow')
+%% estimation
+
+% estimate BVAR
+bvarmf = bvar_(yVAR, lags, options);  
+
+% sort predicted endog vector
+yPredicted = sort(bvarmf.yfill, 3);
+yPredicted = yPredicted(:,1,:);  % select target variable 
+
+% get summary of posteriors
+target = yVAR(:,1);
+pred = median(yPredicted, 3); 
+
+
+%% charts 
+
+% levels
+figure;
+plot(dates, target, 'Linewidth',2, 'marker','o', 'color','r'); 
+hold on; 
+plot(dates, pred, 'Linewidth',2, 'color', 'b')
+legend('observed (quarterly)', 'index (monthly)')
 axis tight
+title('Quarterly Level of Observed vs Index of Economic Activity');
 
-% plot: growth rates
-Dy = 100*(yVAR(4:end,1)-yVAR(1:end-3,1));
-Y = sorty(:,1,0.5*bvarmf.ndraws);
-DY = 100*(Y(4:end,1)-Y(1:end-3,1));
+% growth rates
+targetGrowth = 100*( target(L+1:end,1) - target(1:end-L,1) );
+predGrowth = 100*( pred(L+1:end,1) - pred(1:end-L,1) );
 
-figure('Name','Monthly EA GDP growth')
-plot(dates(4:end),Dy,'ro');
-hold on; plot(dates(4:end),DY,'b','Linewidth',2)
-legend('Quarterly data','MXFREQ VAR flow ')
+figure;
+plot(dates(L+1:end), targetGrowth, 'Linewidth',2, 'marker','o', 'color','r');
+hold on; 
+plot(dates(L+1:end), predGrowth, 'Linewidth',2, 'color', 'b')
+legend('observed (quarterly)', 'index (monthly)')
 axis tight
+title('Quarterly Growth of Observed vs Index of Economic Activity');
